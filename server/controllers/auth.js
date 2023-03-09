b406e210b113df5e10c92402a6cf00c7d7073928
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-03-09 18:48:30 +0530

    login and signup route complete

---
+++ server/controllers/auth.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+export const register = async (req, res) => {
+    try{
+
+        const { firstName, lastName, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({firstName: firstName, lastName: lastName, email, password: hashedPassword });
+        await user.save();
+        res.status(201).send();
+    }
+    catch(err){
+        res.status(500).json({ message: err.message });
+    }
+
+};
+
+export const login = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if(!user){
+            res.status(404).json({ message: "User not found" });
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            res.status(400).json({ message: "Invalid credentials" });
+        }
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({ user, token });
+
+
+    }
+    catch(err){
+        res.status(500).json({ message: err.message });
+    }
+};
--- server/index.js
+++ server/index.js
@@ -5,9 +5,15 @@ import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import multer from 'multer';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import authRoutes from './routes/auth.js';
+
+
 
 
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,5 +21,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan('common'));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  });
+const upload = multer({ storage });
+
+
+app.use("/api/v1/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`);
+    });
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.log(error.message);
+}
+);
 
 
